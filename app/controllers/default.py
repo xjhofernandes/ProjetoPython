07df59ba7c162046f54d6edc26ad31df02f07dac
Jonathan Fernandes <xjhofernandes@gmail.com>
2019-07-13 09:09:04 -0300

    Rotas
    Entendo rotas e utilizando elas.

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,5 +1,15 @@
 from app import app
 
+@app.route("/index")
 @app.route("/") #decorator, é uma caracteristica do python que coloca antes e uma função. Para aplicar uma função em cima de outra
 def index():    #FUnção route em cima da função index '/' é a rota da página
     return "<h1> Hello, World! <h1>"
+
+
+@app.route("/test", defaults={'name' : None})
+@app.route("/test/<name>")#Chamando uma variavel
+def test(name): #2 contrutores? 
+    if name:
+        return "Olar, %s!" % name #imprimindo com a variavel
+    else:
+        return "Olar, usuário"
