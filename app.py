92136c328279b3031d82a4ddce6789bfa7b0fd86
Jonathan Fernandes <xjhofernandes@gmail.com>
2019-07-13 07:59:30 -0300

    Update app.py
    Primeiro Commit para testar o Git Hub.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__) #instância da classe Flask
 
 @app.route("/") #decorator, é uma caracteristica do python que coloca antes e uma função. Para aplicar uma função em cima de outra
 def index():    #FUnção route em cima da função index '/' é a rota da página
-    return "<h1> Hello, World! <h1>"
+    return "<h1> Hello, World! Guys <h1>"
 
 if __name__ == "__main__":
     app.run()
