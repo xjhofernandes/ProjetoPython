1c7faf3ba1d3b7256e7b6f105d538932a816a845
xjhofernandes <xjhofernandes@gmail.com>
2019-07-14 17:57:31 -0300

    site and blok
    Comitando sit e o nl

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,15 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route("/index")
 @app.route("/") #decorator, é uma caracteristica do python que coloca antes e uma função. Para aplicar uma função em cima de outra
 def index():    #FUnção route em cima da função index '/' é a rota da página
-    return "<h1> Hello, World! <h1>"
+    return render_template('index.html')
+
+@app.route("/login")
+def login():
+    return render_template('base.html')
 
 
-@app.route("/test", defaults={'name' : None})
-@app.route("/test/<name>")#Chamando uma variavel
-def test(name): #2 contrutores? 
-    if name:
-        return "Olar, %s!" % name #imprimindo com a variavel
-    else:
-        return "Olar, usuário"
+# @app.route("/test", defaults={'name' : None})
+# @app.route("/test/<name>")#Chamando uma variavel
+# def test(name): #2 contrutores? 
+#     if name:
+#         return "Olar, <b> %s</b> t!" % name #imprimindo com a variavel
+#     else:
+#         return "Olar, usuário"
